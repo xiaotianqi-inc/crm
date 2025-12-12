402fd9c545741c29c072002e218db961587d42de
xiaotianqi <itsupport@zx-studio.com>
2025-12-12 13:13:19 -0500

    crm updated

---
+++ packages/Webkul/Admin/src/Http/Middleware/Locale.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Webkul\Admin\Http\Middleware;
+
+use Closure;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+class Locale
+{
+    /**
+     * @var \Illuminate\Foundation\Application
+     */
+    protected $app; // <-- **FIX: Property Declaration**
+
+    /**
+     * @var \Illuminate\Http\Request
+     */
+    protected $request; // <-- **FIX: Property Declaration**
+
+    /**
+     * The middleware instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        Application $app,
+        Request $request
+    ) {
+        $this->app = $app;
+
+        $this->request = $request;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        app()->setLocale(
+            core()->getConfigData('general.general.locale_settings.locale')
+                ?: app()->getLocale()
+        );
+
+        return $next($request);
+    }
+}
